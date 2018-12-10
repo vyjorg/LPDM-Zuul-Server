5c34e3384199f00b7923a7bf45bc3ec78f59c94d
julien <juliencauwet@yahoo.fr>
2018-12-10 11:51:15 +0100

    first commit

---
+++ src/main/java/com/lpdm/zuulserver/ZuulServerApplication.java
@@ -0,0 +1,12 @@
+package com.lpdm.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+}

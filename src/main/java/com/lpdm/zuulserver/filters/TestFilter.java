1e15fd1c719f8638db53755ed399e0652439ac68
julien <juliencauwet@yahoo.fr>
2019-01-09 16:05:39 +0100

    zuul configuration successful

---
+++ src/main/java/com/lpdm/zuulserver/filters/TestFilter.java
@@ -0,0 +1,37 @@
+package com.lpdm.zuulserver.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+@Component
+public class TestFilter extends ZuulFilter {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        logger.info("test filter : "+RequestContext.getCurrentContext().getRequest().getRequestURI());
+        return null;
+    }
+}
